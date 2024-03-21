e4b60eca763b2c9c8e29eb21fa14247727b2ec0b
Patrick Ongeche <daltonongeche@gmail.com>
2024-03-21 16:46:23 +0400

    Create dart-week4.dart

---
+++ dart-week4.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 15;
+
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
